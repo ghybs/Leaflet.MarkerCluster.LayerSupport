ghybs/Leaflet.MarkerCluster.LayerSupport

commit 8a7bb5a49200580a0393ecc1be8cc9fe119ed648

Fix(chunkedLoading): onAdd & onRemove for proxyLayerGroups
so that they make use of MCG's addLayers & removeLayers (plural) and therefore chunkedLoading.
Fixes #8.

src/layersupport.js
@@ -503,6 +503,14 @@ var _proxyLayerGroup = {
 		delete this._layers[id];
 
 		return this;
+	},
+
+	onAdd: function () {
+		this._proxyMcgLayerSupportGroup.addLayers(this.getLayers());
+	},
+
+	onRemove: function () {
+		this._proxyMcgLayerSupportGroup.removeLayers(this.getLayers());
 	}
 
 };

debug/mcgLayerSupport-controlLayers-realworld.100k.html
@@ -1,0 +1,156 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+
+	<meta charset="utf-8" />
+	<title>Leaflet.MarkerCluster.LayerSupport demo page (Layers Control)</title>
+
+	<link rel="stylesheet" href="https://unpkg.com/leaflet@1.0.3/dist/leaflet.css" />
+	<script src="https://unpkg.com/leaflet@1.0.3/dist/leaflet.js"></script>
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<link rel="stylesheet" href="../docs/examples/screen.css" />
+
+	<link rel="stylesheet" href="https://unpkg.com/leaflet.markercluster@1.0.3/dist/MarkerCluster.css" />
+	<link rel="stylesheet" href="https://unpkg.com/leaflet.markercluster@1.0.3/dist/MarkerCluster.Default.css" />
+	<script src="https://unpkg.com/leaflet.markercluster@1.0.3/dist/leaflet.markercluster-src.js"></script>
+
+	<script src="../dist/leaflet.markercluster.layersupport-src.js"></script>
+	<script src="https://leaflet.github.io/Leaflet.markercluster/example/realworld.50000.1.js"></script>
+	<script src="https://leaflet.github.io/Leaflet.markercluster/example/realworld.50000.2.js"></script>
+
+	<style>
+		#progress {
+			display: none;
+			position: absolute;
+			z-index: 1000;
+			left: 400px;
+			top: 300px;
+			width: 200px;
+			height: 20px;
+			margin-top: -20px;
+			margin-left: -100px;
+			background-color: #fff;
+			background-color: rgba(255, 255, 255, 0.7);
+			border-radius: 4px;
+			padding: 2px;
+		}
+	</style>
+
+</head>
+<body>
+
+<h1>Leaflet.MarkerCluster.LayerSupport demo page</h1>
+
+<h2>
+	Compatibility with
+	<a href="http://leafletjs.com/reference-1.0.2.html#control-layers" target="_blank">L.Control.Layers</a>
+</h2>
+
+<div id="progress">
+	<div id="progress-bar"></div>
+</div>
+<div id="map"></div>
+<input id="add" type="button" value="Add AutoMCG to Map" />
+<input id="remove" type="button" value="Remove AutoMCG from Map" />
+<p>
+	Use the top right Layers Control to dynamically add / remove Layer Groups from the <b>map</b>.
+	Notice that child markers are actually added to / removed from the Marker Cluster Group!
+</p>
+<p>
+	This is the standard
+	<a href="http://leafletjs.com/reference-1.0.2.html#control-layers" target="_blank">Leaflet Layers Control</a>.
+</p>
+<p>
+	Note: it is normal that the computed clusters depend on the order of insertion.
+	This is a result of Leaflet.markercluster algorithm.
+</p>
+<p>
+	<b>Note:</b> if your usage requires only compatibility of MCG with
+	<a href="http://leafletjs.com/reference-1.0.2.html#control-layers" target="_blank">L.Control.Layers</a>,
+	you might be interested in this more simple plugin:
+	<a href="https://github.com/ghybs/Leaflet.FeatureGroup.SubGroup" target="_blank">Leaflet.FeatureGroup.SubGroup</a>.
+</p>
+
+<script type="application/javascript">
+
+	var tiles = L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
+	        maxZoom: 18,
+	        attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors, Points &copy 2012 LINZ'
+	    }),
+	    latlng = L.latLng(-37.82, 175.24);
+
+
+	var map = L.map('map', {center: latlng, zoom: 13, layers: [tiles]});
+
+	var progress = document.getElementById('progress');
+	var progressBar = document.getElementById('progress-bar');
+
+	var mcgLayerSupportGroup = L.markerClusterGroup.layerSupport({
+	        chunkedLoading: true,
+	        chunkInterval: 100,
+	        chunkProgress: updateProgressBar
+	    }),
+	    control = L.control.layers(null, null, { collapsed: false });
+
+	var group1 = L.layerGroup(fillMarkerList(addressPoints)); // 25k points
+	var group2 = L.layerGroup(fillMarkerList(addressPoints2)); // 25k points
+	var group3 = L.layerGroup(fillMarkerList(addressPoints)); // 25k points
+	var group4 = L.layerGroup(fillMarkerList(addressPoints2)); // 25k points
+	//var superGroup = L.layerGroup([group1, group2, group3, group4]);
+
+	mcgLayerSupportGroup.addTo(map);
+
+	mcgLayerSupportGroup.checkIn([group1, group2, group3, group4]);
+
+	control.addOverlay(group1, 'First quarter');
+	control.addOverlay(group2, 'Second quarter');
+	control.addOverlay(group3, 'Third quarter');
+	control.addOverlay(group4, 'Fourth quarter');
+	//control.addOverlay(superGroup, 'Super Group');
+	control.addTo(map);
+
+	group1.addTo(map); // Adding to map or to AutoMCG are now equivalent.
+	group2.addTo(map);
+	group3.addTo(map);
+	group4.addTo(map);
+	//mcgLayerSupportGroup.addLayer(superGroup);
+
+	document.getElementById("add").addEventListener("click", function () {
+		map.addLayer(mcgLayerSupportGroup);
+	});
+
+	document.getElementById("remove").addEventListener("click", function () {
+		map.removeLayer(mcgLayerSupportGroup);
+	});
+
+	function updateProgressBar(processed, total, elapsed, layersArray) {
+		console.log("updateProgressBar");
+		if (elapsed > 200) {
+			// if it takes more than a second to load, display the progress bar:
+			progress.style.display = 'block';
+			progressBar.style.width = Math.round(processed / total * 100) + '%';
+		}
+		if (processed === total) {
+			// all markers processed - hide the progress bar:
+			progress.style.display = 'none';
+		}
+	}
+
+	function fillMarkerList(addressPointsSource) {
+		var markerList = [];
+		for (var i = 0; i < addressPointsSource.length; i++) {
+			var a = addressPointsSource[i];
+			var title = a[2];
+			var marker = L.marker(L.latLng(a[0], a[1]), {
+				title: title
+			});
+			marker.bindPopup(title);
+			markerList.push(marker);
+		}
+		return markerList;
+	}
+
+</script>
+
+</body>
+</html>

commit 74ecf1ddac2df9f1c3e77e915375cf1a1ea26cea

Fix(checkOut): proxy LayerGroups unredirect onAdd and onRemove
so that checking out a Layer Group makes it fully retrieve its normal behaviour.
The bug was introduced by commit 8a7bb5a49200580a0393ecc1be8cc9fe119ed648 which added new `onAdd` and `onRemove` methods to proxy Layer Groups, without them being handled by `_recruitLayerGroupAsProxy` and `_dismissProxyLayerGroup`.

Fixes #14

src/layersupport.js
@@ -335,6 +335,10 @@ L.MarkerClusterGroup.LayerSupport = L.MarkerClusterGroup.extend({
 			layerGroup._originalAddLayer || layerGroup.addLayer;
 		layerGroup._originalRemoveLayer =
 			layerGroup._originalRemoveLayer || layerGroup.removeLayer;
+		layerGroup._originalOnAdd =
+				layerGroup._originalOnAdd || layerGroup.onAdd;
+		layerGroup._originalOnRemove =
+				layerGroup._originalOnRemove || layerGroup.onRemove;
 		L.extend(layerGroup, _proxyLayerGroup);
 	},
 
@@ -350,6 +354,8 @@ L.MarkerClusterGroup.LayerSupport = L.MarkerClusterGroup.extend({
 		delete layerGroup._proxyMcgLayerSupportGroup;
 		layerGroup.addLayer = layerGroup._originalAddLayer;
 		layerGroup.removeLayer = layerGroup._originalRemoveLayer;
+		layerGroup.onAdd = layerGroup._originalOnAdd;
+		layerGroup.onRemove = layerGroup._originalOnRemove;
 
 		var id = L.stamp(layerGroup);
 		delete this._proxyLayerGroups[id];