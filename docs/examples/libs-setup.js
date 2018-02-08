fb03a94bb37c723791ce967feb453d8c61a9c662
ghybs <ghybs1@gmail.com>
2018-02-08 13:47:05 +0400

    Feat(libs-setup): add versions 1.0.1, 1.0.0, 0.1.0 through jsDelivr CDN

--- docs/examples/libs-setup.js
+++ docs/examples/libs-setup.js
@@ -2,8 +2,15 @@
 
 	var cdn = 'https://unpkg.com/';
 	var versionPlaceholder = '@{{VERSION}}/';
+	var libName = 'leaflet.markercluster.layersupport';
 
-	var mcgLayerSupportPathPrefix = cdn + 'leaflet.markercluster.layersupport' + versionPlaceholder + 'dist/';
+	var mcgLayerSupportPathPrefix = cdn + libName + versionPlaceholder + 'dist/';
+
+	var cdn2 = 'https://cdn.jsdelivr.net/';
+	// https://www.jsdelivr.com/features
+	// Template: https://cdn.jsdelivr.net/gh/user/repo@version/file
+	// Note: versions not published on npm (1.0.1, 1.0.0, 0.1.0) were not built, therefore the src file is not in a `dist/` folder.
+	var mcgLayerSupportPathPrefix2 = cdn2 + 'gh/ghybs/' + libName + versionPlaceholder;
 
 	var libs = [
 		global.libLeafletVersions, // Assumes that "leaflet-versions.js" file has already been executed, so that libLeafletVersions is globally available.
@@ -21,7 +28,7 @@
 
 	// Plugin library.
 	libs.push({
-		name: 'leaflet.markercluster.layersupport',
+		name: libName,
 		mandatory: true,
 		versions: [
 			// You can use https://www.srihash.org/ to generate the SRI hash.
@@ -45,7 +52,23 @@
 				name: '1.0.2',
 				sriSrcJs: ''
 			}),
-			// Versions 1.0.1, 1.0.0 and 0.1.0 are not published on npm, hence not available on CDN.
+			// Versions 1.0.1, 1.0.0 and 0.1.0 are not published on npm, hence not available on unpkg CDN.
+			// Use jsDelivr instead.
+			_makeMcgLayerSupportVersionAssets({
+				name: '1.0.1',
+				cdnPrefix: mcgLayerSupportPathPrefix2,
+				sriSrcJs: ''
+			}),
+			_makeMcgLayerSupportVersionAssets({
+				name: '1.0.0',
+				cdnPrefix: mcgLayerSupportPathPrefix2,
+				sriSrcJs: ''
+			}),
+			_makeMcgLayerSupportVersionAssets({
+				name: '0.1.0',
+				cdnPrefix: mcgLayerSupportPathPrefix2,
+				sriSrcJs: ''
+			}),
 			{
 				name: 'local',
 				defaultVersion: true,
@@ -70,13 +93,14 @@
 
 	function _makeMcgLayerSupportVersionAssets(options) {
 		var versionName = options.name;
+		var cdnPrefix = options.cdnPrefix || mcgLayerSupportPathPrefix;
 
 		return {
 			name: versionName,
 			defaultVersion: options.defaultVersion,
 			disabled: options.disabled,
 			assets: [
-				manageLibsVersions.makeScript(mcgLayerSupportPathPrefix + 'leaflet.markercluster.layersupport-src.js', versionName, options.sriSrcJs)
+				manageLibsVersions.makeScript(cdnPrefix + 'leaflet.markercluster.layersupport-src.js', versionName, options.sriSrcJs)
 			]
 		};
 	}
